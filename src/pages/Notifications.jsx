73f550ce19c93e5aaeae38bfa8ab571f5d372831
Alper <alper.demir.fener@gmail.com>
2024-05-02 14:53:05 +0300

    Update Notifications.jsx

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -2,6 +2,8 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import calculateTimeAgo from './../helpers/calculateTimeAgo';
+import { Link } from 'react-router-dom';
+import loadingGIF from "../asset/loading.gif"
 
 const Notifications = () => {
 
@@ -10,6 +12,7 @@ const Notifications = () => {
   const receiverId = useSelector(state => state.user.user.userId);
 
   const [requests, setRequests] = useState([]);
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     getRequests();
@@ -17,11 +20,13 @@ const Notifications = () => {
 
 
   const getRequests = async () => {
+    setLoading(true)
     try {
       const requests = await axios.get(`${URL}/follow-requests/${receiverId}`);
       if (requests) {
         setRequests(requests.data);
         console.log(requests);
+        setLoading(false)
       }
     } catch (error) {
       console.log(error);
@@ -31,7 +36,7 @@ const Notifications = () => {
   const handleAccept = async (requestId) => {
     try {
       await axios.put(`${URL}/accept-follow-request/${requestId}`, {}, { headers: { Authorization: token } });
-      // İsteği kabul ettikten sonra bildirimleri güncelle
+      // Update notifications after accepting the request
       getRequests();
     } catch (error) {
       console.log(error);
@@ -41,7 +46,7 @@ const Notifications = () => {
   const handleReject = async (requestId) => {
     try {
       await axios.put(`${URL}/reject-follow-request/${requestId}`, {}, { headers: { Authorization: token } });
-      // İsteği reddettikten sonra bildirimleri güncelle
+      // Update notifications after rejecting request
       getRequests();
     } catch (error) {
       console.log(error);
@@ -49,32 +54,48 @@ const Notifications = () => {
   }
 
   return (
-    <div>
+    <>
       {
-        requests.length > 0 ? (
-          requests.map(request => (
-            <div key={request._id} className='flex justify-between mt-2 dark:text-white'>
-              <div className='flex gap-x-1 items-center'>
-                <div>
-                  <img src={`${URL}/public/images/${request.senderId.profileUrl}`} alt="img" className='w-9 h-9 rounded-full object-cover min-w-9 min-h-9' />
-                </div>
-                <div className='flex flex-col'>
-                  <span>@{request.senderId.username} wants to follow you</span> <span className='text-[#A7A7A7] dark:text-[#777777] text-xs'>{calculateTimeAgo(request.createdAt)}</span>
-                </div>
+        loading ? (
+          <div className='flex justify-center mt-10'>
+            <img src={loadingGIF} alt="" className='w-20 h-20' />
+          </div>
+        ) :
+          (
+            <div>
+              <div className='mt-3 mb-5 text-center text-lg max-sm:text-base font-bold dark:text-white'>
+                <h3>Notifications {requests.length > 0 && <>({requests.length})</>}</h3>
               </div>
-              <div className='flex gap-x-2 max-sm:gap-x-1 text-sm max-sm:text-xs text-white'>
-                <button onClick={() => handleAccept(request._id)} className='rounded-lg px-4 bg-blue-400 max-sm:px-2 hover:bg-blue-600'>Accept</button>
-                <button onClick={() => handleReject(request._id)} className='rounded-lg px-4 bg-gray-400 max-sm:px-2 hover:bg-gray-600'>Reject</button>
-              </div>
+              {
+                requests.length > 0 ? (
+                  requests.map(request => (
+                    <div key={request._id} className='flex justify-between mt-2 dark:text-white'>
+                      <Link to={`/${request.senderId.username}`} className='flex gap-x-1 items-center'>
+                        <div>
+                          <img src={`${URL}/public/images/${request.senderId.profileUrl}`} alt="img" className='w-9 h-9 rounded-full object-cover min-w-9 min-h-9' />
+                        </div>
+                        <div className='flex flex-col'>
+                          <span>@{request.senderId.username} wants to follow you</span> <span className='text-[#A7A7A7] dark:text-[#777777] text-xs'>{calculateTimeAgo(request.createdAt)}</span>
+                        </div>
+                      </Link>
+                      <div className='flex gap-x-2 max-sm:gap-x-1 text-sm max-sm:text-xs text-white'>
+                        <button onClick={() => handleAccept(request._id)} className='rounded-lg px-4 bg-blue-500 max-sm:px-2 hover:bg-blue-600'>Accept</button>
+                        <button onClick={() => handleReject(request._id)} className='rounded-lg px-4 bg-red-500 max-sm:px-2 hover:bg-red-600'>Reject</button>
+                      </div>
+                    </div>
+                  ))
+                ) : (
+                  <div className='text-center mt-10 bg-gray-100 rounded-md p-3 text-black dark:text-white dark:bg-[#161616] font-bold'>
+                    <span>No notifications</span>
+                  </div>
+                )
+
+              }
+
             </div>
-          ))
-        ) : (
-          <div className='text-center mt-10 bg-blue-300 rounded-md p-3 text-white'>No notifications</div>
-        )
-
+          )
       }
-
-    </div>
+    </>
   )
 }
 
