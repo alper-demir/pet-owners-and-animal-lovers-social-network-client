924722f2575373e4577744665ec35a85b56042d1
Alper <alper.demir.fener@gmail.com>
2024-05-25 01:35:53 +0300

    Update Chat.jsx
    Styling

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -92,6 +92,7 @@ const Chat = () => {
 
         socket.on("message", (message) => {
             setMessages((prevMessages) => [...prevMessages, message]);
+            console.log(message);
         });
 
         return () => {
@@ -117,27 +118,37 @@ const Chat = () => {
     }
 
     return (
-        <div className="flex flex-col h-[80vh] mt-10">
-            <div className='border-b pb-3 flex gap-x-2 items-center'>
+        <div className="flex flex-col h-[80vh] max-sm:h-[78vh] mt-10">
+            <div className='border-b pb-3 flex gap-x-2 items-center dark:text-[#f1f1f1]'>
                 <img src={`${URL}/public/images/${receiverData.profileUrl}`} alt="img" className='w-20 h-20 rounded-full object-cover' />
                 <h3 className="text-xl font-semibold mb-4">Chatting with {receiverData.firstName} <Link to={`/${receiverData.username}`}>(@{receiverData.username})</Link></h3>
-
             </div>
-            <div className="flex-grow overflow-y-auto p-4 mt-2">
+            <div className="flex-grow overflow-y-auto p-4 mt-2 dark:text-[#dbd5d5]">
                 <div className="space-y-2">
                     {messages.map((message, index) => (
                         <>
                             {
                                 message.senderId === currentUserId ? (
                                     <div class="flex justify-end" key={index}>
-                                        <div class="bg-blue-200 text-black p-2 rounded-lg max-w-xs">
-                                            <p>{message.content}</p>
+                                        <div className='flex flex-col'>
+                                            <div class="bg-blue-200 p-2 rounded-lg max-w-xs dark:bg-indigo-700">
+                                                <p>{message.content}</p>
+                                            </div>
+                                            <div className='text-end text-xs'>
+                                                <span>{new Date(message.timestamp).toLocaleTimeString()}</span>
+                                            </div>
                                         </div>
                                     </div>
                                 ) : (
+
                                     <div class="flex" key={index}>
-                                        <div class="bg-gray-300 text-black p-2 rounded-lg max-w-xs">
-                                            <p>{message.content}</p>
+                                        <div className='flex flex-col'>
+                                            <div class="bg-gray-300 p-2 rounded-lg max-w-xs dark:bg-sky-700">
+                                                <p>{message.content}</p>
+                                            </div>
+                                            <div className='text-xs'>
+                                                <span>{new Date(message.timestamp).toLocaleTimeString()}</span>
+                                            </div>
                                         </div>
                                     </div>
                                 )
@@ -147,8 +158,8 @@ const Chat = () => {
                     ))}
                 </div>
             </div>
-            <div class="bg-gray-50 rounded-md p-4 flex items-center">
-                <input type="text" placeholder="Type your message..." class="flex-1 border rounded-full px-4 py-2 focus:outline-none" value={messageInput}
+            <div class="bg-gray-50 rounded-md p-4 flex items-center dark:bg-[#161616] dark:text-[#f1f1f1]">
+                <input type="text" placeholder="Type your message..." class="flex-1 border rounded-full px-4 py-2 focus:outline-none dark:bg-transparent" value={messageInput}
                     onChange={(e) => setMessageInput(e.target.value)} />
                 <button class="bg-blue-500 text-white rounded-full p-2 ml-2 hover:bg-blue-600 focus:outline-none" onClick={handleMessageSend}>
                     <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M11.5003 12H5.41872M5.24634 12.7972L4.24158 15.7986C3.69128 17.4424 3.41613 18.2643 3.61359 18.7704C3.78506 19.21 4.15335 19.5432 4.6078 19.6701C5.13111 19.8161 5.92151 19.4604 7.50231 18.7491L17.6367 14.1886C19.1797 13.4942 19.9512 13.1471 20.1896 12.6648C20.3968 12.2458 20.3968 11.7541 20.1896 11.3351C19.9512 10.8529 19.1797 10.5057 17.6367 9.81135L7.48483 5.24303C5.90879 4.53382 5.12078 4.17921 4.59799 4.32468C4.14397 4.45101 3.77572 4.78336 3.60365 5.22209C3.40551 5.72728 3.67772 6.54741 4.22215 8.18767L5.24829 11.2793C5.34179 11.561 5.38855 11.7019 5.407 11.8459C5.42338 11.9738 5.42321 12.1032 5.40651 12.231C5.38768 12.375 5.34057 12.5157 5.24634 12.7972Z" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
